23adb346d5fcdec30fd497951019ab82e384379d
ornitorrincco <ornitorrincco@gmail.com>
2018-12-03 20:58:23 -0800

    examples fo .net

---
+++ hello-world/helloWorld.cs
@@ -0,0 +1,5 @@
+class MyApp {
+  static void Main() {
+    System.Console.WriteLine("Hello, C#!");
+  }
+}
---
+++ httpClientExample/httpClientExample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+
+
+namespace httpClientExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //#r "System.Net.Http"
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("http://www.microsoft.com/");
+            var response = await client.GetAsync("about");
+            System.Console.WriteLine(response.StatusCode);
+            System.Console.WriteLine(response.Content.Headers.GetValues("Content-Type"));
+            await response.Content.ReadAsStringAsync();
+
+        }
+    }
+}
